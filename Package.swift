84b26f35ccd70e2a36c16eaa4a3144f480dbb7f7
mumuss <michel.musslin@gmail.com>
2019-10-03 16:41:01 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
         .library(name: "PangoCairo", targets: ["PangoCairo"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rhx/SwiftCairo.git", .branch("master")),
-        .package(url: "https://github.com/rhx/SwiftPango.git", .branch("master"))
+        .package(url: "https://github.com/mumuss/SwiftCairo.git", .branch("master")),
+        .package(url: "https://github.com/mumuss/SwiftPango.git", .branch("master"))
     ],
     targets: [
         .target(name: "PangoCairo", dependencies: ["Cairo", "Pango"]),
